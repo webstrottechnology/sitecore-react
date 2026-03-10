db8b14bccd0add549cd56845e4dad215ae620832
poojadasrana <139123204+poojadasrana@users.noreply.github.com>
2026-03-10 16:19:06 +0530

    changes done

--- src/components/shortcode/BlogRightWrap/BlogRightWrapData.jsx
+++ src/components/shortcode/BlogRightWrap/BlogRightWrapData.jsx
@@ -4,91 +4,110 @@ import slider2 from "../../../assets/images/blog-img/blog_image_page_slider2.png
 import slider3 from "../../../assets/images/blog-img/blog_image_page_slider3.png";
 
 export const blogArticles = [
+
+  /* IMAGE BLOG */
   {
     id: 1,
     type: "image",
     img: blogImg1,
-    title: "Navigating Change: Strategies for Corporate Resilience in 2024",
-    desc: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
     date: "25",
     month: "Mar",
+
+    title: "Navigating Change: Strategies for Corporate Resilience in 2024",
+    desc: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+
     author: "admin",
     comments: 2,
     category: "Events",
-    tag: "Multimedia",
+
     link: "/blog-detail",
   },
 
+  /* YOUTUBE BLOG */
   {
     id: 2,
     type: "youtube",
     video: "https://www.youtube.com/embed/ZrGzRmcIFcU",
-    title: "The Power of Collaboration: Fostering a Culture of Teamwork",
-    desc: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
     date: "24",
     month: "Mar",
+
+    title: "The Power of Collaboration: Fostering a Culture of Teamwork",
+    desc: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+
     author: "admin",
     comments: 2,
     category: "Events",
-    tag: "Multimedia",
+
     link: "/blog-detail-video",
   },
 
+  /* VIMEO BLOG */
   {
     id: 3,
     type: "vimeo",
     video: "https://player.vimeo.com/video/306705787?h=84d2ae9af9",
-    title:
-      "Employee Wellness: Prioritizing Health and Happiness in the Workplace - Vimeo video",
-    desc: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
     date: "23",
     month: "Mar",
+
+    title:
+      "Employee Wellness: Prioritizing Health and Happiness in the Workplace - Vimeo Video",
+    desc: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+
     author: "admin",
     comments: 2,
     category: "Events",
-    tag: "Multimedia",
+
     link: "/blog-detail-vimeo",
   },
 
+  /* AUDIO BLOG */
   {
     id: 4,
     type: "audio",
     audio:
       "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1553650693&color=%23ff5500",
+    date: "22",
+    month: "Mar",
+
     title:
       "Corporate Social Responsibility: Making an Impact Beyond Business - Audio",
     desc: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
-    date: "22",
-    month: "Mar",
+
     author: "admin",
     comments: 2,
     category: "Events",
-    tag: "Multimedia",
+
     link: "/blog-detail-audio",
   },
 
+  /* SLIDER BLOG */
   {
     id: 5,
     type: "slider",
     images: [slider1, slider2, slider3],
+    date: "21",
+    month: "Mar",
+
     title:
       "Embracing Diversity and Inclusion: Building a Better Workplace - Slider",
     desc: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
-    date: "21",
-    month: "Mar",
+
     author: "admin",
     comments: 2,
     category: "Events",
-    tag: "Multimedia",
+
     link: "/blog-detail-slider",
   },
 
+  /* QUOTE BLOG */
   {
     id: 6,
     type: "quote",
+
     quote:
       "THE BLINDING SPLENDOR OF THE DIAMOND. THE MIGHTY POWER OF THE ROCKET.",
     author: "Awesome Author",
+
     link: "/blog-detail-quote",
   },
 ];
